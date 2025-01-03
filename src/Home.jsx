4f6a898687632ff387f843ad9a0c45a18a1acd88
Rhys <rhys@rhysjenkins.uk>
2025-01-03 15:31:32 +0000

    Moved data folder, fixed useQuery bug

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
+    const getEuropeData = async () => {
+        const data = await fetch("./data/europe.json");
+        const europeData = await data.json();
+        return europeData;
+    };
+
     const { isPending, error, data } = useQuery({
         queryKey: ["europeData"],
-        queryFn: () => fetch("./data/europe.json").then((res) => res.json()),
+        queryFn: getEuropeData,
     });
-
-    // queryFn: () => fetch("./data/europe.json").then((res) => res.json()),
 
     if (isPending) return "Loading...";
 
