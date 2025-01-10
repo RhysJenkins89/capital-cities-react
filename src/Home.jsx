f7f99ed527b7ed5ead5bcddf9bc98767bcba5a57
Rhys <rhys@rhysjenkins.uk>
2025-01-10 18:24:20 +0000

    Getting useQuery right

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import getEuropeData from "./fetchFunctions/getEuropeData";
 
 const Home = () => {
-    const [currentCountry, getCurrentCountry] = useState("");
+    const [continentData, setContinentData] = useState({});
+    const [currentCountry, setCurrentCountry] = useState("");
 
     const { isPending, error, data } = useQuery({
         queryKey: ["europeData"],
@@ -14,21 +15,37 @@ const Home = () => {
 
     if (error) return "An error has occurred: " + error.message;
 
-    // When should the below function run? Think about the lifecycle of the component.
-    const getRandomCountry = () => {
-        console.log("Europe data:", data);
-        const objectKeys = Object.keys(europeData);
-        console.log("objectKeys", objectKeys);
-        // const randomCountry =
-        //     objectKeys[Math.floor(Math.random() * objectKeys.length)];
-    };
+    // console.log("europeData", data);
+    // console.log("europeData entries", Object.entries(data));
 
-    getRandomCountry();
+    // setContinentData(data);
+
+    // useEffect(() => {
+    //     const getRandomCountry = () => {
+    //         const objectKeys = Object.keys(data);
+    //         console.log("objectKeys", objectKeys);
+    //         const randomCountry =
+    //             objectKeys[Math.floor(Math.random() * objectKeys.length)];
+    //         console.log("random country:", randomCountry);
+    //         setCurrentCountry(randomCountry);
+    //     };
+
+    //     getRandomCountry();
+    // }, []);
+
+    function handleRevealAnswer() {
+        console.log("The user clicked reveal answer.");
+    }
 
     return (
         <div>
             <h1>Capital cities</h1>
             <p>What is the capital of France?</p>
+            <button onClick={handleRevealAnswer}>Reveal answer</button>
+            {/* {Object.entries(data).map((item, index) => {
+                // console.log(item);
+                return <p key={index}>{item[0]}</p>;
+            })} */}
         </div>
     );
 };
