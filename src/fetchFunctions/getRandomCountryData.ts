5bbfa4c7fc8f97c87498879a90c7baf297c1a925
Rhys <rhys@rhysjenkins.uk>
2025-02-04 10:50:43 +0000

    Updating routes to test

--- src/fetchFunctions/getRandomCountryData.ts
+++ src/fetchFunctions/getRandomCountryData.ts
@@ -3,12 +3,15 @@ import CountryData from "../types/CountryData";
 
 // prettier-ignore
 const getRandomCountryData = async (continent: string): Promise<CountryData> => {
+    // I'll need to include auth data with this request. One thing at a time. 
     let url: string;
+    // To test, I'm serving random Europe data.
     if (!continent) {
-        url = `./data/europe.json`;
+        url = `https://cities-api.rhysjenkins.uk/europeRandom`;
     } else {
-        url = `./data/${continent}.json`;
+        url = `https://cities-api.rhysjenkins.uk/europeRandom`;
     }
+    // ./data/${continent}.json
     const data: Response = await fetch(url);
     const continentData: ContinentData = await data.json();
     const objectKeys: string[] = Object.keys(continentData);
