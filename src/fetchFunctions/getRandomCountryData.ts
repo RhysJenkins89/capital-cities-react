996264de44214b8f77e3c55b2635876a93a9e4b7
Rhys <rhys@rhysjenkins.uk>
2025-04-08 23:20:28 +0100

    Added another note

--- src/fetchFunctions/getRandomCountryData.ts
+++ src/fetchFunctions/getRandomCountryData.ts
@@ -1,6 +1,7 @@
 import ContinentData from "../types/ContinentData";
 import CountryData from "../types/CountryData";
 
+// This needs to be refactored. At the moment, I get the data for the whole continent, but then I return the data for only one country, meaning that every time the app re-renders, I get the data for the continent. Instead, I need to get the continent data, save it in state, and then get a random country from that state storage. Or something like that anyway.
 const getRandomCountryData = async (
     continent: string
 ): Promise<CountryData> => {
