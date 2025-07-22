e9a9a356a262af059724c5888aaacdf3387e07d8
Rhys <rhys@rhysjenkins.uk>
2025-07-22 22:03:24 +0100

    Updating user login

--- src/UserLogin.tsx
+++ src/UserLogin.tsx
@@ -1,43 +1,43 @@
 const API_URL = import.meta.env.VITE_API_URL;
 import { useState, FormEvent, useContext } from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { AppContext } from "./Context";
 
 const UserLogin: React.FC = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const handleUserLogin = async (event: FormEvent) => {
-        event.preventDefault();
-        try {
-            const response: Response = await fetch(`${API_URL}/login`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ email, password }),
-            });
+    // const handleUserLogin = async (event: FormEvent) => {
+    //     event.preventDefault();
+    //     try {
+    //         const response: Response = await fetch(`${API_URL}/login`, {
+    //             method: "POST",
+    //             headers: { "Content-Type": "application/json" },
+    //             body: JSON.stringify({ email, password }),
+    //         });
 
-            const userData = await response.json();
-            if (!response.ok) {
-                throw new Error(userData.error);
-            } else {
-                setUserIsLoggedIn(true);
-                console.log("userData:", userData);
-                localStorage.setItem("token", userData.token);
-                console.log("Successfully logged in. Here is the token:", userData.token);
-            }
-            // If the user successfully logs in, I need to show it somehow. However, this state should probably live in the Home component
-            // For now, show the text 'You have logged in.'
-            // Show a signout button
-            // I also need signout functionaltity
-        } catch (error) {
-            console.log("An error occured.");
-            console.log("Error:", error);
-        }
+    //         const userData = await response.json();
+    //         if (!response.ok) {
+    //             throw new Error(userData.error);
+    //         } else {
+    //             setUserIsLoggedIn(true);
+    //             console.log("userData:", userData);
+    //             localStorage.setItem("token", userData.token);
+    //             console.log("Successfully logged in. Here is the token:", userData.token);
+    //         }
+    //         // If the user successfully logs in, I need to show it somehow. However, this state should probably live in the Home component
+    //         // For now, show the text 'You have logged in.'
+    //         // Show a signout button
+    //         // I also need signout functionaltity
+    //     } catch (error) {
+    //         console.log("An error occured.");
+    //         console.log("Error:", error);
+    //     }
 
-        // These two might cause a problem
-        setEmail("");
-        setPassword("");
-    };
+    //     // These two might cause a problem
+    //     setEmail("");
+    //     setPassword("");
+    // };
 
     const context = useContext(AppContext);
 
@@ -59,10 +59,14 @@ const UserLogin: React.FC = () => {
         formState: { errors },
     } = useForm<IFormInput>();
 
+    const handleUserLogin: SubmitHandler<IFormInput> = async (formData: IFormInput) => {
+        console.log("This is the handleUserLogin function.");
+    };
+
     return (
         <div>
-            <form onSubmit={handleUserLogin}>
-                {/* <div>
+            <form onSubmit={handleSubmit(handleUserLogin)}>
+                <div>
                     <label>
                         Email:
                         <input
@@ -76,14 +80,14 @@ const UserLogin: React.FC = () => {
                         />
                         {errors.email && <p>{errors.email.message}</p>}
                     </label>
-                </div> */}
+                </div>
 
-                <div>
+                {/* <div>
                     <label>
                         Email:
                         <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
                     </label>
-                </div>
+                </div> */}
                 <div>
                     <label>
                         Password:
