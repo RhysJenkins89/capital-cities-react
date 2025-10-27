5864ffefab2adc0396d4685e438d1ca4d951b940
Rhys <rhys@rhysjenkins.uk>
2025-10-27 21:46:14 +0000

    Working out context

--- src/Context.tsx
+++ src/Context.tsx
@@ -4,18 +4,26 @@ interface ContextProps {
     children: ReactNode;
 }
 
-interface userLoggedIn {
+// interface userLoggedIn {
+//     userIsLoggedIn: boolean;
+//     setUserIsLoggedIn: (state: boolean) => void;
+// }
+
+type AppContextTypes = {
     userIsLoggedIn: boolean;
     setUserIsLoggedIn: (state: boolean) => void;
-}
+    userData: {};
+    setUserData: () => void;
+};
 
-const AppContext = createContext<userLoggedIn | undefined>(undefined);
+const AppContext = createContext<AppContextTypes | undefined>(undefined);
 
 const AppContextProvider: React.FC<ContextProps> = ({ children }) => {
-    const [userIsLoggedIn, setUserIsLoggedIn] = useState<boolean>(false);
+    const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
+    const [userData, setUserData] = useState();
 
     return (
-        <AppContext.Provider value={{ userIsLoggedIn, setUserIsLoggedIn }}>
+        <AppContext.Provider value={{ userIsLoggedIn, setUserIsLoggedIn, userData, setUserData }}>
             {children}
         </AppContext.Provider>
     );
