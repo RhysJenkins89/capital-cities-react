9e53d2bcd73b748bc816402504199142268a7a6d
Rhys <rhys@rhysjenkins.uk>
2026-02-20 12:29:51 +0000

    Work on frontend user auth

--- src/Home.tsx
+++ src/Home.tsx
@@ -33,7 +33,7 @@ const Home: React.FC = () => {
     // useQuery
     const { isPending, error, data } = useQuery({
         // I should use the error variable here to handle errors. Funny that.
-        queryKey: [continent], // Is this the best name fot the queryKey?
+        queryKey: ["getContinentData"],
         queryFn: () => getContinentData(continent),
         staleTime: Infinity,
         gcTime: Infinity,
