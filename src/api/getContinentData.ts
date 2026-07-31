4dbef8a0d64dcdcb92460f22c40e3200565d62fe
Rhys <rhys@rhysjenkins.uk>
2026-07-31 23:16:59 +0100

    Update types

--- src/api/getContinentData.ts
+++ src/api/getContinentData.ts
@@ -1,17 +1,18 @@
 const API_URL = import.meta.env.VITE_API_URL;
 import CountryData from "../types/CountryData";
+import Continent from "../types/Continent";
 
-const getContinentData = async (continent: string) => {
-    try {
-        let url: string;
-        continent ? (url = `${API_URL}/${continent}`) : (url = `${API_URL}/europe`);
-        const data: Response = await fetch(url);
-        const continentData: CountryData[] = await data.json();
-        return continentData;
-    } catch (error) {
-        console.log("An error occured while fetching the data:", error);
-        throw new Error("This is an error message.");
-    }
+const getContinentData = async (continent: Continent): Promise<CountryData[]> => {
+  // The continent argument shouldn't be any string. It should be one of six strings.
+  try {
+    const url: string = continent ? `${API_URL}/${continent}` : `${API_URL}/europe`;
+    const data: Response = await fetch(url);
+    const continentData: CountryData[] = await data.json();
+    return continentData;
+  } catch (error) {
+    console.log("An error occured while fetching the data:", error);
+    throw new Error("This is an error message.");
+  }
 };
 
 export default getContinentData;
---
+++ src/types/Continent.ts
@@ -0,0 +1,3 @@
+type Continent = "europe" | "asia" | "africa" | "north-america" | "south-america" | "oceania";
+
+export default Continent;
