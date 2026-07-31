7816881e183f60a6ca05ac93db33c000bbf80d5b
Rhys <rhys@rhysjenkins.uk>
2026-07-31 23:17:22 +0100

    Remove comment

--- src/api/getContinentData.ts
+++ src/api/getContinentData.ts
@@ -3,7 +3,6 @@ import CountryData from "../types/CountryData";
 import Continent from "../types/Continent";
 
 const getContinentData = async (continent: Continent): Promise<CountryData[]> => {
-  // The continent argument shouldn't be any string. It should be one of six strings.
   try {
     const url: string = continent ? `${API_URL}/${continent}` : `${API_URL}/europe`;
     const data: Response = await fetch(url);
