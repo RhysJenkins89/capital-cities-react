a9637a24f55bfaa5db22217b2c6f718ad49d7b6a
Rhys <rhys@rhysjenkins.uk>
2026-08-04 07:36:17 +0100

    Remove comment

--- src/utils/capitaliseFirstLetter.ts
+++ src/utils/capitaliseFirstLetter.ts
@@ -1,8 +1,5 @@
 const capitaliseFirstLetter = (input: string): string => {
-    // if (input.length === 0) {
-    //     return; // This function must return a string, but what happens if I pass an empty string into it?
-    // }
-    return input.replace(input[0], input[0].toLocaleUpperCase());
+  return input.replace(input[0], input[0].toLocaleUpperCase());
 };
 
 export default capitaliseFirstLetter;
