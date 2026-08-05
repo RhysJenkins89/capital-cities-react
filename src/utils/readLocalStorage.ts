b2dd4f5457ecf1b24b155ece790dbf1ca5112ca1
Rhys <rhys@rhysjenkins.uk>
2026-08-05 22:30:18 +0100

    Add a comment

--- src/utils/readLocalStorage.ts
+++ src/utils/readLocalStorage.ts
@@ -2,6 +2,7 @@ import continents from "../constants/continents";
 import Continent from "../types/Continent";
 
 function isContinent(localItem: string | null): localItem is Continent {
+  // I need to understand the return type.
   const readOnlyContinents: ReadonlyArray<string> = continents;
   return localItem ? readOnlyContinents.includes(localItem) : false;
 }
