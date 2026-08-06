2c036482fc3a1844b70e1a46799a3e4021d862d8
Rhys <rhys@rhysjenkins.uk>
2026-08-06 07:30:54 +0100

    Add a note

--- src/utils/readLocalStorage.ts
+++ src/utils/readLocalStorage.ts
@@ -2,7 +2,8 @@ import continents from "../constants/continents";
 import Continent from "../types/Continent";
 
 function isContinent(localItem: string | null): localItem is Continent {
-  // I need to understand the return type.
+  // I need to understand the return type. And write down a explanation.
+  // Moreover, what exactly does this return type mean? What does TypeScript see when the data is returned from this function.
   const readOnlyContinents: ReadonlyArray<string> = continents;
   return localItem ? readOnlyContinents.includes(localItem) : false;
 }
