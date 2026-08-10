3c2e4ebc2bb84bc1b9a58f0e2a728ed77513100d
Rhys <rhys@rhysjenkins.uk>
2026-08-10 21:39:01 +0100

    Add minor notes

--- src/api/getContinentData.ts
+++ src/api/getContinentData.ts
@@ -9,8 +9,9 @@ const getContinentData = async (continent: Continent): Promise<CountryData[]> =>
     const continentData: CountryData[] = await data.json();
     return continentData;
   } catch (error) {
-    console.log("An error occured while fetching the data:", error);
-    throw new Error("This is the error message from getContintentData.ts");
+    console.error("An error occured while fetching the data:", error);
+    // throw new Error("This is the error message from getContintentData.ts");
+    // This throw doesn't seem to do anything. Come back to this as well.
     // Note that this line will stop execution of the function and report that something went wrong.
     // Obviously, the error should be more meaningful than this.
   }
--- src/utils/capitaliseFirstLetter.ts
+++ src/utils/capitaliseFirstLetter.ts
@@ -1,5 +1,4 @@
 const capitaliseFirstLetter = (input: string): string => {
-  // return input.replace(input[0], input[0].toLocaleUpperCase());
   return input.charAt(0).toLocaleUpperCase() + input.slice(1);
 };
 
