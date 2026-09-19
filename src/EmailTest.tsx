c96629068f9adc49614939efac542e8f72fa5af2
Rhys <rhys@rhysjenkins.uk>
2026-09-19 09:28:43 +0100

    Add cors error note

--- src/EmailTest.tsx
+++ src/EmailTest.tsx
@@ -3,10 +3,17 @@ const API_URL = import.meta.env.VITE_API_URL;
 const EmailTest = () => {
   const handleButtonClick = async () => {
     console.log("handle button click");
-    const response: Response = await fetch(`${API_URL}/email`);
+    const response: Response = await fetch(`http://localhost:8080`);
     const data = await response.json();
     console.log("data:", data);
   };
+
+  // Understand the following error:
+  // Access to fetch at 'http://localhost:8080/' from origin 'http://localhost:5173' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+
+  // Also, I need a method for sorting out these thoughts that come into my mind as I'm building. It might be a Newportian-style working-memory.txt file.
+
+  // This looks like a useful resource: https://www.stackhawk.com/blog/golang-cors-guide-what-it-is-and-how-to-enable-it/.
 
   return (
     <div>
